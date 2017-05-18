8f24fd7d9ca2b0925c79a6d3069c01b342bac20b
Timothy <tim@gotomyhome.com>
2017-05-18 15:32:18 +0000

    12 done

--- db/migrate/20170516194124_create_quotes.rb
+++ db/migrate/20170516194124_create_quotes.rb
@@ -1,8 +1,8 @@
 class CreateQuotes < ActiveRecord::Migration[5.0]
   def change
     create_table :quotes do |t|
-      t.string:saying
-      t.string:author
+      t.string :saying
+      t.string :author
       t.timestamps
     end
   end
