6fabf7649c2252d4eafa9c8fefcf7946ba4fa2a4
Timothy <tim@gotomyhome.com>
2017-05-22 17:21:33 +0000

    16

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,10 +2,6 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
-
-  def new
-    @quote = Quote.new
-  end
   
   def create
     Quote.create(quote_params)
